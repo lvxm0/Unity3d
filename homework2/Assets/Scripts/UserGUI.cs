e149567dfcaf5ad537ad2263065313a5c27cff6d
lvxm0 <31623469+lvxm0@users.noreply.github.com>
2018-04-04 03:10:29 +0800

    Update UserGUI.cs

--- homework2/Assets/Scripts/UserGUI.cs
+++ homework2/Assets/Scripts/UserGUI.cs
@@ -18,15 +18,15 @@ namespace homework
             action = Director.getInstance().currentSceneController as UserAction;
 
             style = new GUIStyle();
-            style.fontSize = 40;
-            style.alignment = TextAnchor.MiddleCenter;
+            style.fontSize = 30;//字号
+            style.alignment = TextAnchor.MiddleCenter;//居中
 
             buttonStyle = new GUIStyle("button");
-            buttonStyle.fontSize = 30;
+            buttonStyle.fontSize = 20;
         }
         void OnGUI()
         {
-            if (status == 1)
+            if (status == 1)//win
             {
                 GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 85, 100, 50), "Gameover!", style);
                 if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2, 140, 70), "Restart", buttonStyle))
@@ -35,7 +35,7 @@ namespace homework
                     action.restart();
                 }
             }
-            else if (status == 2)
+            else if (status == 2)//lose
             {
                 GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 85, 100, 50), "You win!", style);
                 if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2, 140, 70), "Restart", buttonStyle))
