261cf0cc130eb98ab1a8e141fd09fda5e87f8ff3
lvxm0 <31623469+lvxm0@users.noreply.github.com>
2018-06-07 00:59:06 +0800

    Add files via upload

---
+++ bloodbar/subUGUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class subUGUI : MonoBehaviour {
+    public Slider slider;
+    // Use this for initialization
+    void Start()
+    {
+        Button Subbtn = this.GetComponent<Button>();
+        Subbtn.onClick.AddListener(Click);
+    }
+
+    public void Click()
+    {
+        if (slider.value > 0)
+        {
+            slider.value -= 0.1f;
+        }
+        else
+        {
+            slider.value = 0;
+        }
+    }
+}
